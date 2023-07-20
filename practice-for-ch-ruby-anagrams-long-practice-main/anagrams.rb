def0f76a8cf8e623cf38d44daaacc53c6e6cf1e6
AkashPulluru <akashpulluru@gmail.com>
2023-07-20 14:47:31 -0400

    second anagram done

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -19,6 +19,28 @@ def first_anagram?(str_1, str_2)
     finalarray.any? {|word| word == str_2}
 
 end
-# p first_anagram?("gizmo", "sally")
-# p first_anagram?("elvis", "lives")
 
+def second_anagram?(str_1, str_2)
+    array = [] 
+    str_1.each_char do |char|
+        array << char 
+    end
+    
+    tempstring = ""
+
+    i = 0 
+
+    while i < str_2.length 
+        if !array.include?(str_2[i])
+            tempstring += str_2[i]
+        end 
+        i += 1
+    end  
+
+    tempstring.length == 0
+end 
+
+
+
+p second_anagram?("gizmo", "sally")
+p second_anagram?("elvis", "lives")
