f23591330d0bac62810f3bb6904f84ea66d56f7e
Sean <pca.sean.jeun@gmail.com>
2023-07-20 14:15:44 -0400

    #first_anagram? method ph1 built

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -1,3 +1,24 @@
-def anagrams?(str_1, str_2)
+def first_anagram?(str_1, str_2)
+    newarr = []
+
+    str_1.each_char.with_index do |char, idx|
+        newarr << idx 
+    end 
+
+    stringarray = newarr.permutation.to_a
+    finalarray = []
+
+    stringarray.each do |subarray|
+        tempstring = "" 
+        subarray.each do |index|
+           tempstring += str_1[index]
+        end 
+        finalarray << tempstring 
+    end 
+
+    finalarray.any? {|word| word == str_2}
 
 end
+# p first_anagram?("gizmo", "sally")
+# p first_anagram?("elvis", "lives")
+
