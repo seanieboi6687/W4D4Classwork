a10a89743a27f04d815b5acf612e46bd428e8aff
Sean <pca.sean.jeun@gmail.com>
2023-07-20 16:19:14 -0400

    two_sum? bruteforce built

--- practice-for-ch-ruby-two-sum-long-practice-main/two_sum.rb
+++ practice-for-ch-ruby-two-sum-long-practice-main/two_sum.rb
@@ -1,3 +1,16 @@
-def two_sum?
+def two_sum?(arr, target_sum)
 
+    arr.each_with_index do |num1, idx1|
+        arr.each_with_index do |num2, idx2|
+            if idx2 > idx1 && num1 + num2 == target_sum
+                return true
+            end
+        end
+    end
+
+    return false
 end
+
+arr = [0, 1, 5, 7]
+p two_sum?(arr, 6)
+p two_sum?(arr, 10)
