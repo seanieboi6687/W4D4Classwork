67c2dfc6556384956ba1f7413ce8560238eaf66c
AkashPulluru <akashpulluru@gmail.com>
2023-07-20 16:24:58 -0400

    two sum second problem

--- practice-for-ch-ruby-two-sum-long-practice-main/two_sum.rb
+++ practice-for-ch-ruby-two-sum-long-practice-main/two_sum.rb
@@ -11,6 +11,20 @@ def two_sum?(arr, target_sum)
     return false
 end
 
+def okay_two_sum(arr, target_sum)
+newarr = arr.sort 
+
+i = 0 
+
+while i < newarr.length - 2 
+    if newarr[i] + newarr[i+1] == target_sum 
+        return true 
+    end 
+    i += 1 
+end 
+return false 
+end 
+
 arr = [0, 1, 5, 7]
-p two_sum?(arr, 6)
-p two_sum?(arr, 10)
+p two_sum?(arr, 6) # true 
+p two_sum?(arr, 10) # false 
